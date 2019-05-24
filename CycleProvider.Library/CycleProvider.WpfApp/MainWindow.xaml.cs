a3302bbc7fec8e7db3dfd4bad4dbaaeed3caf8ef
Dariusz Popis <dariusz.popis@comfuture.pl>
2019-05-24 12:14:23 +0200

    Wpf Styles, BackgroundWorker - simple demos

--- CycleProvider.Library/CycleProvider.WpfApp/MainWindow.xaml.cs
+++ CycleProvider.Library/CycleProvider.WpfApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using CycleProvider.Library;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,26 +19,50 @@ namespace CycleProvider.WpfApp
 {
     public partial class MainWindow : Window
     {
-        private CycleProvider<Point> _provider = new CycleProvider<Point>();
+        private CycleProvider<Point> _cycleProvider = new CycleProvider<Point>();
+        private BackgroundWorker _bw = new BackgroundWorker();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            _provider.Add(new Point { X = 1, Y = 1 });
-            _provider.Add(new Point { X = 0, Y = 1 });
-            _provider.Add(new Point { X = 0, Y = 0 });
-            _provider.Add(new Point { X = 1, Y = 0 });
-            _provider.Add(new Point { X = 0, Y = 0 });
-            _provider.Add(new Point { X = 0, Y = 1 });
-            _provider.Next();
+            SetupCycleProvider();
+            SetupElpFloat();
+            SetupBackgroundWorker();
+            BtnStart.Click += (s, a) => { if (!_bw.IsBusy) _bw.RunWorkerAsync(); BtnStart.IsHitTestVisible = false; };
+        }
 
-            ElpFloat.DataContext = _provider;
+        private void SetupElpFloat()
+        {
+            ElpFloat.DataContext = _cycleProvider;
             ElpFloat.MouseEnter += (s, a) =>
             {
-                _provider.Next();
-                Title = _provider.CurrentItem.ToString();
+                _cycleProvider.Next();
+                Title = _cycleProvider.CurrentItem.ToString();
             };
+        }
+
+        private void SetupCycleProvider()
+        {
+            _cycleProvider.Add(new Point { X = 1, Y = 1 });
+            _cycleProvider.Add(new Point { X = 0, Y = 1 });
+            _cycleProvider.Add(new Point { X = 0, Y = 0 });
+            _cycleProvider.Add(new Point { X = 1, Y = 0 });
+            _cycleProvider.Add(new Point { X = 0, Y = 0 });
+            _cycleProvider.Add(new Point { X = 0, Y = 1 });
+            _cycleProvider.Next();
+        }
+
+        private void SetupBackgroundWorker()
+        {
+            _bw.DoWork += (s, a) =>
+              {
+                  while (true)
+                  {
+                      _cycleProvider.Next();
+                      1000.Sleep();
+                  }
+              };
         }
 
         private struct Point
